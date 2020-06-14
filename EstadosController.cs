15941e9940a9d01ffb6ca8d6b8847acb0531df7e
universoti <64614511+universoti@users.noreply.github.com>
2020-06-14 00:41:59 -0300

    Add files via upload

---
+++ EstadosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using webestados.Models.Estados;
+
+namespace webestados.Controllers.Estados
+{
+    public class EstadosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult EstadoPesquisado(string id)
+        {           
+            EstadosViewModel selecionado = new EstadosViewModel();
+            selecionado = new ListarEstadosViewModel().ListarEstadosPesq().Where(c => c.Sigla == id).FirstOrDefault();
+
+            ViewBag.Sigla = selecionado.Sigla;
+            ViewBag.Descricao = selecionado.NomeEstado;
+            ViewBag.Aliquota = selecionado.Aliquota;
+            
+            return View();
+        }
+    }
+}
