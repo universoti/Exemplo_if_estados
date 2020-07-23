ba71b485036a6130124d1efa8acc5417f79cc0ec
universoti <erickfrodrigues@gmail.com>
2020-07-23 00:46:38 +0000

    teste 2

--- EstadosController.cs
+++ EstadosController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +11,7 @@ namespace webestados.Controllers.Estados
     {
         public IActionResult Index()
         {
-            var steste="";
+            var steste="1";
             return View();
         }
 
